5a34ea4c03b5bae65579065670cd929efb472e50
Fekete <brunohffekete@gmail.com>
2022-11-30 16:19:29 -0300

    feat: get booking

--- src/controllers/bookings-controller.ts
+++ src/controllers/bookings-controller.ts
@@ -1,11 +1,13 @@
 import { AuthenticatedRequest } from "@/middlewares";
+import bookingService from "@/services/bookings-service";
 import { Response } from "express";
 import httpStatus from "http-status";
 
 export async function getBooking(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
   try {
-    return res.sendStatus(httpStatus.OK);
+    const booking = await bookingService.getBooking(Number(userId));
+    return res.status(httpStatus.OK).send(booking);
   } catch (error) {
     return res.sendStatus(httpStatus.NOT_FOUND);
   }
